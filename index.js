f4625ed66de1cc717730bd8d67ca6cc0947ac601
Ejjedkins <127382824+Ejjedkins@users.noreply.github.com>
2025-03-06 02:18:42 -0600

    Update index.js
    Creating a static triangle shaped player.

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ canvas.height = window.innerHeight
 
 canvas.fillStyle = 'black'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+class Player {
+    constructor ({position, velocity}) {
+        this.position = position // Position is a x, y object
+        this.velocity = velocity
+    }
+    //Designing the player
+    shape() {
+
+        ctx.moveTo(this.position.x + 30, this.position.y)
+        ctx.lineTo(this.position.x - 10, this.position.y - 10)
+        ctx.lineTo(this.position.x - 10, this.position.y + 10)
+        ctx.closePath()
+
+        ctx.strokeStyle = 'white'
+        ctx.stroke()
+    }
+
+}
+
+const player  = new Player( {
+    position: {x:canvas.width / 2 , y :canvas.height / 2}, //Will place charater in the middle of the screen
+    velocity:{x:0 , y :0}
+})
+
+player.shape()
+
+console.log(player)
