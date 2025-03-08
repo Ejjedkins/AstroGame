48363a640163482a7a426d9580e46a0b930bae39
Ejjedkins <127382824+Ejjedkins@users.noreply.github.com>
2025-03-08 01:06:52 -0600

    Update index.js
    Allowing player to rotate with left and right keys

--- index.js
+++ index.js
@@ -13,11 +13,17 @@ class Player {
     constructor ({position, velocity}) {
         this.position = position // Position is a x, y object
         this.velocity = velocity
+        this.rotation = 0
     }
     //Designing the player
     shape() {
-        //Preventing a snail trail from player
-        ctx.beginPath()
+        ctx.save()
+        
+        ctx.translate(this.position.x, this.position.y)
+        ctx.rotate(this.rotation)
+        ctx.translate(-this.position.x, -this.position.y)
+        
+        ctx.beginPath() // This wreventing a snail trail from player
         ctx.moveTo(this.position.x + 30, this.position.y)
         ctx.lineTo(this.position.x - 10, this.position.y - 10)
         ctx.lineTo(this.position.x - 10, this.position.y + 10)
@@ -25,6 +31,7 @@ class Player {
 
         ctx.strokeStyle = 'white'
         ctx.stroke()
+        ctx.restore
     }
 
     //Update the position for every frame based on the velocity of x & y
@@ -46,6 +53,14 @@ player.shape()
 const keys = {
     ArrowUp: {
         pressed: false
+    },
+
+    ArrowRight: {
+        pressed: false
+    },
+
+    ArrowLeft: {
+        pressed: false
     }
 }
 
@@ -57,8 +72,11 @@ function animate() {
     
     player.update()
 
-
+    player.velocity.x = 0 // Allows the player to stop if key is released
     if (keys.ArrowUp.pressed) player.velocity.x = 1
+        
+    if (keys.ArrowRight.pressed) player.rotation += 0.01
+        else if (keys.ArrowLeft.pressed) player.rotation -= 0.01 //Will rotate player the opposite way of right key
 
 }
 
@@ -66,16 +84,30 @@ animate()
 
 window.addEventListener('keydown', (evt) =>  {
     switch (evt.code){
-        //Prints out when up, left, and right arrows are pressed on cosole log
         case 'ArrowUp':
-            console.log('up arrow was pressed') 
             keys.ArrowUp.pressed = true 
             break
         case 'ArrowLeft':
-            console.log('left arrow was pressed')   
+            keys.ArrowLeft.pressed = true 
             break
         case 'ArrowRight':
-            console.log('right arrow was pressed')   
+            keys.ArrowRight.pressed = true   
+            break
+    }
+
+} )
+
+//Creating an event listener for when the player releases a key
+window.addEventListener('keyup', (evt) =>  {
+    switch (evt.code){
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false  
             break
     }
 
